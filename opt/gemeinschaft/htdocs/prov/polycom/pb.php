64d799adfff1d9f4e0e2a36d24bd055ffac6228c
Henning Holtschneider <henning@loca.net>
2010-10-24 16:15:44 +0200

    Apply group permission restrictions on imported phonebook items

--- opt/gemeinschaft/htdocs/prov/polycom/pb.php
+++ opt/gemeinschaft/htdocs/prov/polycom/pb.php
@@ -158,8 +158,8 @@ if(!$type)
 			case "gs" :
 				$cq .= "`users` WHERE `nobody_index` IS NULL AND `pb_hide` = 0";
 				break;
-			case "imported" :
-				$cq .= "`pb_ldap`";
+			case "imported":
+				$cq .= '`pb_ldap` WHERE `group_id` IN ('. implode(',', $user_groups) .')' ;
 				break;
 			case "prv" :
 				$cq .= "`pb_prv` WHERE `user_id`=". $user_id;
@@ -228,7 +228,9 @@ $num_results = (int) gs_get_conf("GS_POLYCOM_PROV_PB_NUM_RESULTS", 10);
 
 if($type === "imported")
 {
-	// we don't need $user for this
+	$user = trim( @$_REQUEST['u'] );
+	$user_id = getUserID( $user );
+	$user_groups = gs_group_members_groups_get(array($user_id), 'user');
 
 	ob_start();
 
@@ -248,6 +250,8 @@ if($type === "imported")
 	echo "<html>\n";
 	echo "<head><title>". $pagetitle ."</title></head>\n";
 	echo "<body><br />\n";
+
+	$searchsql .= ($searchsql ? ' AND ' : ' ') . '`group_id` IN ('. implode(',', $user_groups) .')';
 
 	$query =
 		"SELECT `lastname` `ln`, `firstname` `fn`, `number` `ext` ".
